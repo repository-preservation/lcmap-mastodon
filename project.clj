5d30037ec20501eb96be8b34b2fa2c2eb68e2460
caustin-usgs <cgaustin@contractor.usgs.gov>
2017-10-17 14:16:53 -0500

    remove speclj dependency. dont want to bother with phantomjs

--- project.clj
+++ project.clj
@@ -5,25 +5,27 @@
             :url  ""}
 
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [org.clojure/core.async "0.3.443"]
                  [cljs-http "0.1.43"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-3308"]
-                                  [speclj "3.3.1"]]}}
-  :plugins [[speclj "3.3.1"]
-            [lein-cljsbuild "1.0.5"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-3308"]]}}
 
-  :cljsbuild {:builds        {:dev  {:source-paths   ["src/cljs" "spec/cljs"]
+  :plugins [[lein-cljsbuild "1.0.5"]]
+
+  :cljsbuild {:builds        {:dev  {:source-paths   ["src/cljs"]
                                      :compiler       {:output-to     "js/lcmap-mastodon_dev.js"
                                                       :optimizations :whitespace
                                                       :pretty-print  true}
-                                     :notify-command ["phantomjs"  "bin/speclj" "js/lcmap-mastodon_dev.js"]}
+                                     ;;:notify-command ["js/lcmap-mastodon_dev.js"]
+                                     }
 
                               :prod {:source-paths ["src/cljs"]
                                      :compiler     {:output-to     "js/lcmap-mastodon.js"
                                                     :optimizations :simple}}}
-              :test-commands {"test" ["phantomjs" "bin/speclj" "js/lcmap-mastodon_dev.js"]}}
+              ;;:test-commands {"test" ["phantomjs" "bin/speclj" "js/lcmap-mastodon_dev.js"]}
+              }
 
   :source-paths ["src/cljs"]
-  :test-paths ["spec/cljs"]
+  ;;:test-paths ["spec/cljs"]
 
   :aliases {"cljs" ["do" "clean," "cljsbuild" "once" "dev"]})
