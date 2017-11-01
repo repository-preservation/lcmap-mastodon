bf0748d871e6b91d8fb44358046027ee23fde509
clay austin <clay.austin.ctr@usgs.gov>
2017-11-01 16:22:46 -0500

    initial funcs to deal with difference in inventory reporting between chipmunk and ard store

--- src/lcmap/mastodon/http.cljs
+++ src/lcmap/mastodon/http.cljs
@@ -9,12 +9,15 @@
 (defn get-request
  "Wrapper func for async HTTP GET requests
 
-  ^String :url: URL to request
-  ^Hash Map :params: Query parameters
+  ^String   :url:  URL to request
+  ^Hash Map :resp: Optional desired response, used for testing
  "
- [url]
- (go (def response (<! (http-client/get url {:with-credentials? false}))))
- (:body response)
+ [url & [resp]]
+ (if (nil? resp)
+     (do   
+       (go (def response (<! (http-client/get url {:with-credentials? false}))))
+       (:body response))
+     (do resp)) 
 )
 
 
