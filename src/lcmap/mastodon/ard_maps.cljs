9f2068d7d5467a458dbf7261f719f9eb3f03a145
clay austin <clay.austin.ctr@usgs.gov>
2017-11-03 09:02:41 -0500

    replace faux http request method for tests with more dynamic wrapper around cljs-http request method. move test data into its own namespace. add ard-maps namespace for succinctly storing what ARD source tar holds for contents

---
+++ src/lcmap/mastodon/ard_maps.cljs
@@ -0,0 +1,9 @@
+(ns lcmap.mastodon.ard-maps)
+
+(def tar-map
+
+  (hash-map :QA '("LINEAGEQA" "PIXELQA" "RADSATQA" "SRAEROSOLQA")
+            :SR '("SRB1" "SRB2" "SRB3" "SRB4" "SRB5" "SRB6" "SRB7" )
+            :BT '("BTB10" "BTB11")
+            :TA '("TAB1" "TAB2" "TAB3" "TAB4" "TAB5" "TAB6" "TAB7" "TAB8" "TAB9"))
+)
