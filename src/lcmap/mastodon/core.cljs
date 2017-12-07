6736080954ce71180e0a3bf5ea2a36722b54a95c
caustin-usgs <cgaustin@contractor.usgs.gov>
2017-12-07 13:55:54 -0600

    re-org how funcs are parked on channels

--- src/lcmap/mastodon/core.cljs
+++ src/lcmap/mastodon/core.cljs
@@ -8,10 +8,11 @@
             [cljs.core.async :refer [<! >! chan]]))
 
 
-(def ard-chan (chan 1)) ;; channel used for determing ARD status
-(def rpt-chan (chan 1)) ;; channel used for reporting ARD status
-(def ing-chan (chan 1)) ;; channel used to handle ARD to ingest
-(def sts-chan (chan 1)) ;; channel used to handle ingest status
+(def ard-chan (chan 30)) ;; channel used for determing ARD status
+(def idw-chan (chan 30)) ;; random channel 
+(def rpt-chan (chan 30)) ;; channel used for reporting ARD status
+(def ing-chan (chan 30)) ;; channel used to handle ARD to ingest
+(def sts-chan (chan 30)) ;; channel used to handle ingest status
 
 (def ard-miss-atom (atom {})) ;; atom containing list of ARD not yet ingested
 (def idw-miss-atom (atom {})) ;; atom containing list of ARD only found in IDWS
@@ -55,19 +56,11 @@
   (str host "/inventory?tile=" tile-id)
 )
 
-(defn ard-status-check 
-  "Check whether available ARD has been ingested. If it hasn't
-   place it in the requested Atom.
-
-   ^ core.async.chan :ard-c:
-   ^ String :idw-url:
-   ^ Func :idw-rqt:"
-  [ard-c idw-url idw-rqt busy-div ingest-btn ing-ctr mis-ctr & [dom-func rchan]]
+(defn idw-handler [idw-c idw-url idw-rqt busy-div ingest-btn ing-ctr mis-ctr & [dom-func rchan]]
   (go
-    (let [ard-tars (<! ard-c)
-          ard-tifs (set (flatten (map ard/ard-manifest ard-tars))) 
-          idw-resp (<! (idw-rqt idw-url))   
-          idw-tifs (set (util/collect-map-values idw-resp :source)) 
+    (let [ard-tifs (<! idw-c)
+          idw-resp (<! (idw-rqt idw-url))
+          idw-tifs (set (util/collect-map-values idw-resp :source))
           ard-only (set/difference ard-tifs idw-tifs)
           idw-only (set/difference idw-tifs ard-tifs)
           ingested (set/intersection ard-tifs idw-tifs)
@@ -86,7 +79,24 @@
           (swap! idw-miss-atom assoc :tifs idw-only)
           (util/log (str "missing count: " (count (:tifs (deref ard-miss-atom)))))
           (dom-updt rptr-map)
-          (>! rptr-chn rptr-map)))
+          (>! rptr-chn rptr-map)
+    )
+  )
+
+)
+
+(defn ard-status-check
+  [ard-c idw-c]
+  (go
+    (let [ard-tars (<! ard-c)
+          ard-tifs (set (flatten (map ard/ard-manifest ard-tars)))]
+
+        ;; put idw response on idw-chan, make sure theres a func parked on idw-c
+        ;; that handles the diff work of the original ard-status-check
+        (>! idw-c ard-tifs)
+        ;;(util/log (str "ard-tifs: " ard-tifs))
+        )
+  )
 )
 
 (defn inventory-diff
@@ -112,8 +122,10 @@
          ;; put ard and idw resource paths on atoms
          (swap! ard-host-atom assoc :path ard-url)
          (swap! idw-host-atom assoc :path (str idw-host "/inventory"))
-         ;; park functions on ard-chan
-         (ard-status-check ard-chan idw-url idw-rqt bsy-div ing-btn ing-ctr mis-ctr)
+         ;; park func on idw-chan, when an item is put on this chan, doe the diff work
+         (idw-handler idw-chan idw-url idw-rqt bsy-div ing-btn ing-ctr mis-ctr)
+         ;; park func on ard-chan, when an ard response is received, put it on the idw-chan
+         (ard-status-check ard-chan idw-chan)
          ;; put items on ard-chan
          (go                 
            (>! ard-chan (-> (<! (ard-rqt ard-url))
@@ -125,7 +137,9 @@
   [ing-c idw-path sts-c]
   (go
     (let [tifs (<! ing-c)]
+      (util/log (str "tifs: " tifs))
       (doseq [t tifs]
+        (util/log (str "ingest tif: " t))
         (>! sts-c  (<! (http/post-request idw-path {"url" t}))))))
 )
 
