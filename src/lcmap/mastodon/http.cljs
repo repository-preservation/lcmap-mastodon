15884cc461c265f0072d7660d18b481641489036
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-01-10 08:46:20 -0600

    cleanup and formatting for http.cljs

--- src/lcmap/mastodon/http.cljs
+++ src/lcmap/mastodon/http.cljs
@@ -2,50 +2,36 @@
    "HTTP Request functions"
    (:require-macros [cljs.core.async.macros :refer [go]])
    (:require [cljs-http.client :as http-client]
-             [cljs.core.async :refer [<!]]
-             [lcmap.mastodon.data :as testdata]))
+             [cljs.core.async :refer [<!]]))
 
-(enable-console-print!)
-
-(defn log [msg]
+(defn log 
+  "Function for logging messages to the JS console.
+   ^String :msg: The message to log to the console"
+  [msg]
   (.log js/console msg))
 
 (defn get-request
- "Wrapper func for async HTTP GET requests
+  "Wrapper func for async HTTP GET requests
+   ^String   :url:  URL to request
 
-  ^String   :url:  URL to request
-  ^Hash Map :resp: Optional desired response, used for testing
- "
- [url & [resp]]
- (if (nil? resp)
-    (do (go (let [response (<! (http-client/get url {:with-credentials? false}))]
-               (:body response)))) 
-    (do resp)) 
-)
+   Returns a Core.Async Channel"
+  [url & [resp]]
+  (if (nil? resp)
+     (do (go (let [response (<! (http-client/get url {:with-credentials? false}))]
+                  (:body response)))) 
+     (do resp)))
 
 (defn post-request
   "Wrapper func for async HTTP POST requests
-
    ^String   :url: URL to post to
    ^Hash Map :data: Data to be posted
-   ^Hash Map :resp: Optional desired response, used for testing"
+   
+   Returns a Core.Async Channel"
   [url data & [resp]]
   (if (nil? resp)
-    (do 
-      (go
-        (let [response (<! (http-client/post url {:with-credentials? false
-                                                  :json-params data
-                                                  :headers {"Content-Type" "application/json"
-                                                            "Accept" "application/json"}}))]
-        response)))
-
-    (do resp))
-)
-
-(defn mock-ard []
-  #(get-request % (testdata/ard-resp)))
-
-(defn mock-idw []
-  #(get-request % (testdata/idw-resp)))
-
+    (do (go (let [headers  {"Content-Type" "application/json" "Accept" "application/json"}
+                  params   {:with-credentials? false :json-params data :headers headers}
+                  response (<! (http-client/post url params))]
+             response)))
+    (do resp)))
 
