04b83b64921af4b9034b4a493f665215ef7d983a
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-01-18 10:09:14 -0600

    add parameter for alternative ingest host

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@ INDEX=/usr/share/nginx/html/index.html
 
 sed -i "s!<ARDHOST>!${ARD_HOST}!g" ${INDEX}
 sed -i "s!<IWDSHOST>!${IWDS_HOST}!g" ${INDEX}
+sed -i "s!<INGESTHOST>!${INGEST_HOST}!g" ${INDEX}
 
 /usr/sbin/nginx -g 'daemon off;'
