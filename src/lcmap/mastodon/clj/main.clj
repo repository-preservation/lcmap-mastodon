a1ddae49a3ac1f79ebc99b392690080120676cb9
clay austin <clay.austin.ctr@usgs.gov>
2018-03-12 09:01:26 -0500

    move server responsibilities into own namespace

--- src/lcmap/mastodon/clj/main.clj
+++ src/lcmap/mastodon/clj/main.clj
@@ -1,65 +1,18 @@
 (ns lcmap.mastodon.clj.main
   (:gen-class)
-  (:require [clojure.string                 :as string]
-            [compojure.core                 :as compojure]
-            [compojure.route                :as route]
-            [cheshire.core                  :refer :all]
+  (:require [cheshire.core                  :refer :all]
             [environ.core                   :as environ]
-            [lcmap.mastodon.cljc.ard        :as ard]
             [lcmap.mastodon.cljc.util       :as util]
-            [lcmap.mastodon.clj.file        :as file]
             [lcmap.mastodon.clj.persistance :as persist]
             [lcmap.mastodon.clj.validation  :as validation]
             [org.httpkit.client             :as http]
             [org.httpkit.server             :as server]
-            [ring.middleware.json           :as ring-json]))
+            [lcmap.mastodon.clj.server      :as mserver]))
 
 (defn pmap-partitions 
   [infunc collection]
   (doseq [i collection]
     (doall (pmap infunc i))))
-
-(defn bulk-ingest
-  "Generate ingest requests for list of posted ARD"
-  [{:keys [:body] :as req}]
-  (let [tifs    (string/split (:urls body) #",")
-        iwds    (:iwds-host environ/env)
-        results (doall (pmap #(persist/ingest % iwds) tifs))]
-    {:status 200 :body results}))
-
-(defn ard-status
-  [tileid]
-  (let [hvmap    (util/hv-map tileid)
-        filepath (-> (:ard-path environ/env) (str (:h hvmap) "/" (:v hvmap) "/*"))
-        ardtifs  (-> filepath (file/get-filenames)
-                              (util/with-suffix "tar")
-                              (#(map ard/ard-manifest %))
-                              (flatten))
-        iwds_src (-> (:iwds-host environ/env) (str "/inventory?only=source&source="))
-        ing_src  (-> (:ard-host environ/env) (str "/ard"))
-        ard_res  (doall (pmap #(persist/status-check % iwds_src ing_src) ardtifs))]
-
-    (let [missing  (filter (fn [i] (= (vals i) '("[]"))) ard_res)
-          ingested (filter (fn [i] (not (= (vals i) '("[]")))) ard_res)
-          miss_count   (count missing)
-          ingest_count (count ingested)
-          miss_flat (keys (apply merge-with concat missing))]
-      {:status 200 :body {:ingested ingest_count :missing miss_flat}})))
-
-(defn get-base [request]
-  {:status 200 :body ["Would you like some ARD with that?"]})
-
-;; ## Routes
-(compojure/defroutes routes
-  (compojure/context "/" request
-    (route/resources "/")
-    (compojure/GET   "/" [] (get-base request))
-    (compojure/GET   "/inventory/:tileid{[0-9]{6}}" [tileid] (ard-status tileid))
-    (compojure/POST  "/bulk-ingest" [] (bulk-ingest request))))
-
-(def app (-> routes
-             (ring-json/wrap-json-body {:keywords? true})
-             (ring-json/wrap-json-response)))
 
 (defn -main [& args]
   (let [tileid          (first args)
@@ -74,7 +27,7 @@
         (when (not (validation/validate-server iwds_host ard_host partition_level ard_path)) 
           (println "validation failed, exiting")
           (System/exit 0))
-        (server/run-server #'app {:port 9876}))
+        (server/run-server #'mserver/app {:port 9876}))
       (do
         (when (not (validation/validate-cli tileid iwds_host ard_host partition_level))
           (println "validation failed, exiting")
@@ -88,7 +41,7 @@
           (let [response_map   (-> (:body @ard_response) (parse-string true))
                 missing_vector (:missing response_map)
                 ingested_count (:ingested response_map)
-                ard_partition (partition partition_level partition_level "" missing_vector)
+                ard_partition  (partition partition_level partition_level "" missing_vector)
                 ingest_map #(persist/ingest % iwds_host)]
 
             (println "Tile Status Report for: " tileid)
---
+++ src/lcmap/mastodon/clj/server.clj
@@ -0,0 +1,53 @@
+(ns lcmap.mastodon.clj.server
+   (:require [clojure.string                 :as string]
+             [compojure.core                 :as compojure]
+             [compojure.route                :as route]
+             [environ.core                   :as environ]
+             [lcmap.mastodon.cljc.ard        :as ard]
+             [lcmap.mastodon.cljc.util       :as util]
+             [lcmap.mastodon.clj.file        :as file]
+             [lcmap.mastodon.clj.persistance :as persist]
+             [ring.middleware.json           :as ring-json]))
+
+
+(defn bulk-ingest
+  "Generate ingest requests for list of posted ARD"
+  [{:keys [:body] :as req}]
+  (let [tifs    (string/split (:urls body) #",")
+        iwds    (:iwds-host environ/env)
+        results (doall (pmap #(persist/ingest % iwds) tifs))]
+    {:status 200 :body results}))
+
+(defn ard-status
+  [tileid]
+  (let [hvmap    (util/hv-map tileid)
+        filepath (-> (:ard-path environ/env) (str (:h hvmap) "/" (:v hvmap) "/*"))
+        ardtifs  (-> filepath (file/get-filenames)
+                              (util/with-suffix "tar")
+                              (#(map ard/ard-manifest %))
+                              (flatten))
+        iwds_src (-> (:iwds-host environ/env) (str "/inventory?only=source&source="))
+        ing_src  (-> (:ard-host environ/env) (str "/ard"))
+        ard_res  (doall (pmap #(persist/status-check % iwds_src ing_src) ardtifs))]
+
+    (let [missing  (filter (fn [i] (= (vals i) '("[]"))) ard_res)
+          ingested (filter (fn [i] (not (= (vals i) '("[]")))) ard_res)
+          miss_count   (count missing)
+          ingest_count (count ingested)
+          miss_flat (keys (apply merge-with concat missing))]
+      {:status 200 :body {:ingested ingest_count :missing miss_flat}})))
+
+(defn get-base [request]
+  {:status 200 :body ["Would you like some ARD with that?"]})
+
+;; ## Routes
+(compojure/defroutes routes
+  (compojure/context "/" request
+    (route/resources "/")
+    (compojure/GET   "/" [] (get-base request))
+    (compojure/GET   "/inventory/:tileid{[0-9]{6}}" [tileid] (ard-status tileid))
+    (compojure/POST  "/bulk-ingest" [] (bulk-ingest request))))
+
+(def app (-> routes
+             (ring-json/wrap-json-body {:keywords? true})
+             (ring-json/wrap-json-response)))
