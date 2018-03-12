b42d73ddb7f3d7dd68d6218dd9243d0ad21f0094
clay austin <clay.austin.ctr@usgs.gov>
2018-03-12 11:24:32 -0500

    multi-arity for main instead of if/else

--- src/lcmap/mastodon/clj/main.clj
+++ src/lcmap/mastodon/clj/main.clj
@@ -9,53 +9,49 @@
             [org.httpkit.server             :as server]
             [lcmap.mastodon.clj.server      :as mserver]))
 
+(def iwds_host       (:iwds-host environ/env))
+(def ard_host        (:ard-host  environ/env))
+(def ard_path        (:ard-path  environ/env))
+(def partition_level (read-string (:partition-level environ/env)))        
+
 (defn pmap-partitions 
   [infunc collection]
   (doseq [i collection]
     (doall (pmap infunc i))))
 
-(defn -main [& args]
-  (let [tileid          (first args)
-        autoingest      (last  args)
-        iwds_host       (:iwds-host environ/env)
-        ard_host        (:ard-host  environ/env)
-        ard_path        (:ard-path  environ/env)
-        partition_level (read-string (:partition-level environ/env))]
+(defn -main
+  ([]
+    (when (not (validation/validate-server iwds_host ard_host partition_level ard_path)) 
+      (println "validation failed, exiting")
+      (System/exit 1))
+    (server/run-server #'mserver/app {:port 9876}))
+  ([tileid & args]
+    (when (not (validation/validate-cli tileid iwds_host ard_host partition_level))
+      (println "validation failed, exiting")
+      (System/exit 1))
+    (let [ard_response   (http/get (util/ard-url-format ard_host tileid))
+          response_map   (-> (:body @ard_response) (parse-string true))
+          missing_vector (:missing response_map)
+          ard_partition  (partition partition_level partition_level "" missing_vector)
+          ingest_map     #(persist/ingest % iwds_host)
+          autoingest     args]
 
-    (if (nil? tileid)
-      (do ;; no args, run server
-        (when (not (validation/validate-server iwds_host ard_host partition_level ard_path)) 
-          (println "validation failed, exiting")
-          (System/exit 1))
-        (server/run-server #'mserver/app {:port 9876}))
-      (do
-        (when (not (validation/validate-cli tileid iwds_host ard_host partition_level))
-          (println "validation failed, exiting")
-          (System/exit 1))
-
-        (let [ard_response   (http/get (util/ard-url-format ard_host tileid))
-              response_map   (-> (:body @ard_response) (parse-string true))
-              missing_vector (:missing response_map)
-              ard_partition  (partition partition_level partition_level "" missing_vector)
-              ingest_map    #(persist/ingest % iwds_host)]
-
-          (println "Tile Status Report for: " tileid)
-          (println "To be ingested: " (count missing_vector))
-          (println "Already ingested: " (:ingested response_map))
-          (println "")
+      (println "Tile Status Report for: " tileid)
+      (println "To be ingested: " (count missing_vector))
+      (println "Already ingested: " (:ingested response_map))
+      (println "")
           
-          (if (= autoingest "-y")
-            (do 
+      (if (= autoingest "-y")
+        (do 
+          (pmap-partitions ingest_map ard_partition)
+          (println "Ingest Complete"))
+        (do 
+          (println "Ingest? (y/n)")
+          (if (= (read-line) "y")
+            (do
               (pmap-partitions ingest_map ard_partition)
               (println "Ingest Complete"))
             (do 
-              (println "Ingest? (y/n)")
-              (if (= (read-line) "y")
-                (do
-                  (pmap-partitions ingest_map ard_partition)
-                  (println "Ingest Complete"))
-                (do 
-                  (println "Exiting!")
-                  (System/exit 0))))))
-        (System/exit 0)))))
+              (println "Exiting!"))))))
+    (System/exit 0)))
 
