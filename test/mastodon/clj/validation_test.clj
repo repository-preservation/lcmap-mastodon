a95f0b2a850591d1af0d09af71106fe41c6af5c4
clay austin <clay.austin.ctr@usgs.gov>
2018-03-13 15:38:38 -0500

    add validation ns test

---
+++ test/mastodon/clj/validation_test.clj
@@ -0,0 +1,21 @@
+(ns mastodon.clj.validation-test
+  (:require [clojure.test :refer :all] 
+            [mastodon.clj.validation :as validation]))
+
+(deftest not-nil?-test
+  (is (= (validation/not-nil? 9 "foo") true)))
+
+(deftest does-match?-test
+  (is (= (validation/does-match? #"[0-9]{6}" "005015" "TileID") true)))
+
+(deftest is-int?-test
+  (is (= (validation/is-int? 9 "Foo") true)))
+
+(deftest validate-cli-test
+  (is (= (validation/validate-cli "005015" "iwdshost" "ardhost" 10) true)))
+
+(deftest validate-server-test
+  (is (= (validation/validate-server "iwdshost" "ardhost" 10 "/tmp/foo/") true)))
+
+
+
