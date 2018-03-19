8f477a42eaa32adb59e62459dafba2f0202d226d
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-03-19 08:41:27 -0500

    fix server test for new validation

--- test/mastodon/clj/server_test.clj
+++ test/mastodon/clj/server_test.clj
@@ -3,7 +3,8 @@
             [mastodon.clj.server :as server]
             [mastodon.clj.file :as file]
             [ring.mock.request :as mock]
-            [mastodon.clj.persistance :as persist]))
+            [mastodon.clj.persistance :as persist]
+            [mastodon.clj.validation :as validation]))
 
 (deftest bulk-ingest-test
   (with-redefs [persist/ingest (fn [a b] "tif")]
@@ -12,6 +13,10 @@
 
 (deftest ard-status-test
   (with-redefs [file/get-filenames (fn [path x] ["LE07_CU_005015_20021221_20170919_C01_V01_BT.tar"])
-                persist/status-check (fn [tif x y] {"LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "[]"})]
+                persist/status-check (fn [tif x y] {"LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "[]"})
+                validation/http-accessible? (fn [x] true)
+                server/ard-host "http://ardhost.gov"
+                server/iwds-host "http://iwdshost.gov"]
+
     (is (= (server/ard-status "005015")
            {:status 200, :body {:ingested 0, :missing '("LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif")}}))))
