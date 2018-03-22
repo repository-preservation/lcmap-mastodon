7b99cb0681d7290eb64d86acbee87a267d7eca94
clay austin <clay.austin.ctr@usgs.gov>
2018-03-22 10:39:07 -0500

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,6 @@ sed -i "s!<IWDSHOST>!${IWDS_HOST}!g" ${INDEX}
 sed -i "s!<INGESTHOST>!${ARD_HOST}!g" ${INDEX}
 sed -i "s!<INGESTPARTITIONING>!${PARTITION_LEVEL}!g" ${INDEX}
 
-nohup java -jar /usr/local/bin/lcmap-mastodon-0.1.13-standalone.jar &
+nohup java -jar /usr/local/bin/lcmap-mastodon-1.0.0-standalone.jar &
 /usr/sbin/nginx -g 'daemon off;'
 
