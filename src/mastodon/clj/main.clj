06bd0e599fab3ee83ded77f08c50a968e16e21e8
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-03-26 15:36:50 -0500

    better logging and exception handling in clj main and validation

--- src/mastodon/clj/main.clj
+++ src/mastodon/clj/main.clj
@@ -1,13 +1,14 @@
 (ns mastodon.clj.main
   (:gen-class)
-  (:require [cheshire.core            :refer :all]
+  (:require [cheshire.core            :refer [parse-string]]
             [environ.core             :as environ]
             [mastodon.cljc.util       :as util]
             [mastodon.clj.persistance :as persist]
             [mastodon.clj.validation  :as validation]
             [org.httpkit.client       :as http]
             [org.httpkit.server       :as server]
-            [mastodon.clj.server      :as mserver]))
+            [mastodon.clj.server      :as mserver]
+            [clojure.tools.logging    :as log]))
 
 (def iwds_host       (:iwds-host environ/env))
 (def ard_host        (:ard-host  environ/env))
@@ -24,32 +25,45 @@
 (defn -main
   ([]
     (if (validation/validate-server iwds_host ard_host partition_level ard_path)
-      (do (server/run-server #'mserver/app {:port 9876})) 
-      (do (println "validation failed, exiting")
+      (do (try
+            (server/run-server #'mserver/app {:port 9876})
+            (catch Exception ex
+              (log/error ex "error starting Mastodon")))) 
+      (do (log/error "validation failed, exiting")
           (System/exit 1))))
   ([tileid & args]
     (when (not (validation/validate-cli tileid iwds_host ard_host partition_level))
-      (println "validation failed, exiting")
+      (log/error "validation failed, exiting")
       (System/exit 1))
-    (let [ard_response   (http/get (util/ard-url-format ard_host tileid))
-          response_map   (-> (:body @ard_response) (parse-string true))
-          missing_vector (:missing response_map)
-          ard_partition  (partition partition_level partition_level "" missing_vector)
-          ingest_map     #(persist/ingest % iwds_host)
-          autoingest     (first args)]
+    (try
+      (let [ard_response   (http/get (util/ard-url-format ard_host tileid))
+            response_map   (-> (:body @ard_response) (parse-string true))
+            missing_vector (:missing response_map)
+            ard_partition  (partition partition_level partition_level "" missing_vector)
+            ingest_map     #(persist/ingest % iwds_host)
+            autoingest     (first args)]
 
-      (println "Tile Status Report for: " tileid)
-      (println "To be ingested: " (count missing_vector))
-      (println "Already ingested: " (:ingested response_map))
-      (println "")
+        (when (:error @ard_response)
+          (log/error (str "Error reaching ARD_HOST: " (:error @ard_response)))
+          (System/exit 1))
 
-      (if (= autoingest "-y")
-        (do (pmap-partitions ingest_map ard_partition)
-            (println "Ingest Complete"))
-        (do (println "Ingest? (y/n)")
-            (if (= (read-line) "y")
-              (do (pmap-partitions ingest_map ard_partition)
-                  (println "Ingest Complete"))
-              (do (println "Exiting!"))))))
+        (log/info (str "\nTile Status Report for: " tileid "\n"
+                       "To be ingested: " (count missing_vector) "\n"
+                       "Already ingested: " (:ingested response_map) "\n"))
+
+        (if (= autoingest "-y")
+          (do (pmap-partitions ingest_map ard_partition)
+              (log/info "Ingest Complete"))
+          (do (println "Ingest? (y/n)")
+              (if (= (read-line) "y")
+                (do (pmap-partitions ingest_map ard_partition)
+                    (println "Ingest Complete"))
+                (do (println "Exiting!"))))))
+      (catch com.fasterxml.jackson.core.JsonParseException jx
+        (log/error jx "Non-json response from ARD_HOST request")
+        (System/exit 1))
+      (catch Exception ex
+        (log/error ex "Error determining tile ingest status")
+        (System/exit 1)))
     (System/exit 0)))
 
