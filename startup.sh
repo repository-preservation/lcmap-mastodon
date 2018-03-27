b0ebca4523776b3430cf3e0ccfd800ee2ebeff47
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-03-27 11:55:34 -0500

    leave version definition to only the project.clj

--- startup.sh
+++ startup.sh
@@ -2,10 +2,12 @@
 
 INDEX=/usr/share/nginx/html/index.html
 
+VERSION=`head -n1 project.clj | grep -oP "[\d]+\.[\d]+\.[\d]+(-SNAPSHOT)?(-RC[\echo+])?"`
+
 sed -i "s!<ARDHOST>!${ARD_HOST}!g" ${INDEX}
 sed -i "s!<IWDSHOST>!${IWDS_HOST}!g" ${INDEX}
 sed -i "s!<INGESTHOST>!${ARD_HOST}!g" ${INDEX}
 sed -i "s!<INGESTPARTITIONING>!${PARTITION_LEVEL}!g" ${INDEX}
 
-nohup java -jar /lcmap-mastodon-1.0.0-standalone.jar & /usr/sbin/nginx -g 'daemon off;'
+nohup java -jar /lcmap-mastodon-${VERSION}-standalone.jar & /usr/sbin/nginx -g 'daemon off;'
 
