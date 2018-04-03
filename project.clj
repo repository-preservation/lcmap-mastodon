e0d65c2c0f0478cd66da7dc168a757ee92c66a23
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-04-03 10:16:26 -0500

    enable filtering for available ARD by acquired year

--- project.clj
+++ project.clj
@@ -24,6 +24,7 @@
                  [http-kit.fake             "0.2.1"]
                  [lein-doo                  "0.1.8"]
                  [ring                      "1.6.3"]
+                 [ring/ring-defaults        "0.3.1"]
                  [ring/ring-json            "0.4.0"]
                  [ring/ring-jetty-adapter   "1.6.3"]
                  [ring/ring-mock            "0.3.2"]]
--- src/mastodon/clj/server.clj
+++ src/mastodon/clj/server.clj
@@ -10,7 +10,9 @@
              [mastodon.clj.file        :as file]
              [mastodon.clj.persistance :as persist]
              [mastodon.clj.validation  :as validation]
-             [ring.middleware.json     :as ring-json]))
+             [ring.middleware.json     :as ring-json]
+             [ring.middleware.keyword-params :as ring-keyword-params]
+             [ring.middleware.defaults :as ring-defaults]))
 
 (def iwds-host (:iwds-host environ/env))
 (def ard-host  (:ard-host  environ/env))
@@ -24,6 +26,7 @@
     {:status 200 :body results}))
 
 (defn http-deps-check
+  "Return error response if external http dependencies are not reachable"
   []
   (let [ard-accessible  (validation/http-accessible? ard-host "ARD_HOST")
         iwds-accessible (validation/http-accessible? iwds-host "IWDS_HOST")
@@ -36,35 +39,6 @@
            (= true iwds-accessible) {:error ard-message}
            :else {:error (str ard-message iwds-message)})))))
 
-(defn ard-status-orig
-  "Determine the ARD ingest status for the given tile id."
-  [tileid]
-  (let [ard-accessible  (validation/http-accessible? ard-host "ARD_HOST")
-        iwds-accessible (validation/http-accessible? iwds-host "IWDS_HOST")
-        ard-message  (str "ARD Host: " ard-host " is not reachable. ") 
-        iwds-message (str "IWDS Host: " iwds-host " is not reachable")]
-
-    (if (= #{true} (set [ard-accessible iwds-accessible]))
-      (do (try
-            (let [hvmap    (util/hv-map tileid)
-                  filepath (str ard-path (:h hvmap) "/" (:v hvmap) "/*")
-                  ardtifs  (-> filepath (file/get-filenames "tar")
-                               (#(map ard/ard-manifest %))
-                               (flatten))
-                  ard_res  (doall (pmap #(persist/status-check % iwds-host (str ard-host "/ard")) ardtifs))
-                  missing  (-> ard_res (#(filter (fn [i] (= (vals i) '("[]"))) %)) 
-                               (#(apply merge-with concat %)) 
-                               (keys))
-                  ingested_count (- (count ard_res) (count missing))]
-              {:status 200 :body {:ingested ingested_count :missing missing}})
-            (catch Exception ex
-              (log/errorf "Error determining tile: %s ARD status. exception: %s" (.getMessage ex))
-              {:status 200 :body {:error (format "Error determining tile: %s ARD status. exception: %s" (.getMessage ex))}})))
-      (do (cond
-           (= true ard-accessible)  {:status 200 :body {:error iwds-message}}
-           (= true iwds-accessible) {:status 200 :body {:error ard-message}}
-           :else {:status 200 :body {:error (str ard-message iwds-message)}})))))
-
 (defn available-ard
   "Return a vector of available ARD for the given tile id"
   [tileid]
@@ -74,46 +48,37 @@
                  (#(map ard/ard-manifest %))
                  (flatten))))
 
-(defn available-ard-filtered
+(defn filtered-ard
+  "Return vector of available ARD for a given tile id, between the provided years"
   [tileid from to]
   (let [ardtifs (available-ard tileid)
-        froms   (filter (fn [i] (>= (-> i (util/tif-only) (ard/year-acquired) (read-string)) from-i)) ardtifs)
-        tos     (filter (fn [i] (<= (-> i (util/tif-only) (ard/year-acquired) (read-string)) to-i)) ardtifs)]
+        froms   (filter (fn [i] (>= (-> i (util/tif-only) (ard/year-acquired) (read-string)) from)) ardtifs)
+        tos     (filter (fn [i] (<= (-> i (util/tif-only) (ard/year-acquired) (read-string)) to)) ardtifs)]
     (vec (set/intersection (set froms) (set tos)))))
 
 (defn ard-report
   "Return hash-map of missing ARD and an ingested count"
   [tifs]
-  (let [ard_res  (doall (pmap #(persist/status-check % iwds-host (str ard-host "/ard")) ardtifs))
+  (let [ard_res  (doall (pmap #(persist/status-check % iwds-host (str ard-host "/ard")) tifs))
         missing  (-> ard_res (#(filter (fn [i] (= (vals i) '("[]"))) %)) 
                              (#(apply merge-with concat %)) 
                              (keys))
-        ingested_count (- (count ard_res) (count missing))])
-  {:missing missing :ingested ingested_count})
+        ingested_count (- (count ard_res) (count missing))]
+  {:missing missing :ingested ingested_count}))
 
 (defn ard-status
-  ([tileid]
-   (try
-     (let [ardtifs (available-ard tileid)
-           report  (ard-report ardtifs)
-           deps-check (http-deps-check)]
-       (if (nil? (:error deps-check))
-         {:status 200 :body report}
-         {:status 200 :body {:error (:error deps-check)}}))
+  [tileid {:keys [params] :as req}]
+  (try
+     (let [from (or (util/try-string (:from params)) 0)
+           to   (or (util/try-string (:to params)) 3000)
+           tifs (filtered-ard tileid from to)
+           deps (http-deps-check)]
+       (if (nil? (:error deps))
+         {:status 200 :body (ard-report tifs)}
+         {:status 200 :body {:error (:error deps)}}))
       (catch Exception ex
-        (log/errorf "Error determining tile: %s ARD status. exception: %s" (.getMessage ex))
-        {:status 200 :body {:error (format "Error determining tile: %s ARD status. exception: %s" (.getMessage ex))}})))
-  ([tileid from to]
-   (try
-     (let [ardtifs (available-ard-filtered tileid from to)
-           report  (ard-report ardtifs)
-           deps-check (http-deps-check)]
-       (if (nil? (:error deps-check))
-         {:status 200 :body report}
-         {:status 200 :body {:error (:error deps-check)}}))
-      (catch Exception ex
-        (log/errorf "Error determining tile: %s ARD status. exception: %s" (.getMessage ex))
-        {:status 200 :body {:error (format "Error determining tile: %s ARD status. exception: %s" (.getMessage ex))}}))))
+        (log/errorf "Error determining tile: %s ARD status. exception: %s" tileid (.getMessage ex))
+        {:status 200 :body {:error (format "Error determining tile: %s ARD status. exception: %s" tileid (.getMessage ex))}})))
 
 (defn get-base 
   "Hello Mastodon"
@@ -124,10 +89,11 @@
   (compojure/context "/" request
     (route/resources "/")
     (compojure/GET   "/" [] (get-base request))
-    (compojure/GET   "/inventory/:tileid{[0-9]{6}}" [tileid] (ard-status tileid))
-    (compojure/GET   "/inventory/:tileid{[0-9]{6}}/:from{[0-9]{6}}/:to{[0-9]{6}}" [tileid from to] (ard-status tileid from to))
+    (compojure/GET   "/inventory/:tileid{[0-9]{6}}" [tileid] (ard-status tileid request))
     (compojure/POST  "/bulk-ingest" [] (bulk-ingest request))))
 
 (def app (-> routes
              (ring-json/wrap-json-body {:keywords? true})
-             (ring-json/wrap-json-response)))
+             (ring-json/wrap-json-response)
+             (ring-defaults/wrap-defaults ring-defaults/api-defaults)
+             (ring-keyword-params/wrap-keyword-params)))
