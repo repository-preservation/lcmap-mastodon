6a8a7d5f8d5598643c5a6ac1b44da3a209627048
clay austin <clay.austin.ctr@usgs.gov>
2018-04-17 15:14:49 -0500

    update tests

--- test/mastodon/clj/server_test.clj
+++ test/mastodon/clj/server_test.clj
@@ -1,4 +1,5 @@
 (ns mastodon.clj.server-test
+  (:use org.httpkit.fake)
   (:require [clojure.test :refer :all]
             [clojure.string :as string]
             [mastodon.clj.server :as server]
@@ -29,6 +30,18 @@
     (is (= {:error "ARD Host: ard-host.com is not reachable. IWDS Host: iwds-host.com is not reachable"}
            (server/http-deps-check)))))
 
+(deftest available-ard-test
+  (with-redefs [file/get-filenames (fn [a b] ["LC08_CU_005015_20130415_20171016_C01_V01_SR.tar" "LT05_CU_005015_19840508_20170912_C01_V01_BT.tar"])] 
+    (is (= (server/available-ard "005015")
+        '("LC08_CU_005015_20130415_20171016_C01_V01_SRB2.tif"
+          "LC08_CU_005015_20130415_20171016_C01_V01_SRB3.tif" 
+          "LC08_CU_005015_20130415_20171016_C01_V01_SRB4.tif" 
+          "LC08_CU_005015_20130415_20171016_C01_V01_SRB5.tif" 
+          "LC08_CU_005015_20130415_20171016_C01_V01_SRB6.tif" 
+          "LC08_CU_005015_20130415_20171016_C01_V01_SRB7.tif" 
+          "LC08_CU_005015_20130415_20171016_C01_V01_PIXELQA.tif" 
+          "LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif")))))
+
 (deftest filtered-ard-test
   (with-redefs [server/available-ard (fn [i] tiflist)]
     (is (= (server/filtered-ard "005015" 1983 1994)
@@ -49,4 +62,22 @@
 
     (is (= (server/ard-status "005015" {})
            {:status 200, :body {:ingested 0, :missing '("LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif")}}))))
+
+(deftest aux-status-test
+  (with-fake-http [{:url "http://auxhost.com/aux" :method :get} {:status 200 :body "<html><head>auxhead</head><body>AUX_CU_005015_20000731_20171031_V01.tar</body></html>"}]
+    (with-redefs [server/aux-host "http://auxhost.com/aux"
+                  server/http-deps-check (fn [] {})
+                  server/data-report (fn [a b] {:missing ["AUX_CU_005015_20000731_20171031_V01_ASPECT.tif" "AUX_CU_005015_20000731_20171031_V01_POSIDEX.tif" "AUX_CU_005015_20000731_20171031_V01_TRENDS.tif"] :ingested 3})]
+      (is (= (server/aux-status "005015")
+             {:status 200 
+              :body {:missing ["AUX_CU_005015_20000731_20171031_V01_ASPECT.tif" "AUX_CU_005015_20000731_20171031_V01_POSIDEX.tif" "AUX_CU_005015_20000731_20171031_V01_TRENDS.tif"] 
+                     :ingested 3}})))))
+
+(deftest get-status-test
+  (with-redefs [server/server-type "ard"
+                server/ard-status (fn [a b] "hello from ard-status")]
+    (is (= (server/get-status "005015" {}) "hello from ard-status")))
+  (with-redefs [server/server-type "aux"
+                server/aux-status (fn [a] "hello from aux-status")]
+    (is (= (server/get-status "005015" {}) "hello from aux-status"))))
 
--- test/mastodon/cljc/data_test.clj
+++ test/mastodon/cljc/data_test.clj
@@ -2,10 +2,14 @@
   (:require [clojure.test :refer :all] 
             [mastodon.cljc.data :as data]))
 
-(deftest tar-name-test
+(deftest ard-tar-name-test
   (let [tif-name "LC08_CU_022010_20131211_20171016_C01_V01_PIXELQA.tif"
         tar-name "LC08_CU_022010_20131211_20171016_C01_V01_SR.tar"]
     (is (= tar-name (data/ard-tar-name tif-name)))))
+
+(deftest aux-tar-name-test
+  (is (= (data/aux-tar-name "AUX_CU_005015_20000731_20171031_V01_ASPECT.tif")
+         "AUX_CU_005015_20000731_20171031_V01.tar")))
 
 (deftest ard-manifest-test
   (let [l5resp '("LT05_CU_022010_19841204_20170912_C01_V01_SRB1.tif"
@@ -25,7 +29,19 @@
     (is (= l5resp (data/ard-manifest "LT05_CU_022010_19841204_20170912_C01_V01_SR.tar")))
     (is (= l8resp (data/ard-manifest "LC08_CU_022010_19841204_20170912_C01_V01_SR.tar")))))
 
+(deftest aux-manifest-test
+  (is (= (data/aux-manifest "AUX_CU_005015_20000731_20171031_V01.tar")
+         '("AUX_CU_005015_20000731_20171031_V01_ASPECT.tif" "AUX_CU_005015_20000731_20171031_V01_DEM.tif" "AUX_CU_005015_20000731_20171031_V01_MPW.tif" "AUX_CU_005015_20000731_20171031_V01_POSIDEX.tif" "AUX_CU_005015_20000731_20171031_V01_SLOPE.tif" "AUX_CU_005015_20000731_20171031_V01_TRENDS.tif"))))
+
 (deftest tar-path-test
   (is (= "oli_tirs/ARD_Tile/2002/CU/005/015"
          (data/tar-path "LC08_CU_005015_20021221_20170919_C01_V01_BT.tar"))))
+
+(deftest date-acquired-test
+  (is (= (data/date-acquired "LC08_CU_022010_19841204_20170912_C01_V01_PIXELQA.tif")
+         "19841204")))
+
+(deftest year-acquired-test
+  (is (= (data/year-acquired "LC08_CU_022010_19841204_20170912_C01_V01_PIXELQA.tif")
+         "1984")))
 
