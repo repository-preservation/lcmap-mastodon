510ef7454edd610417c3ba471cc3279ab8ea41eb
clay austin <clay.austin.ctr@usgs.gov>
2018-05-01 16:26:20 -0500

    fix aux host labeling in index.html

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lcmap-mastodon "1.1.1"
+(defproject lcmap-mastodon "1.1.2"
   :description "Functions for LCMAP data curation"
   :url "https://eros.usgs.gov"
   :license {:name "Unlicense"
