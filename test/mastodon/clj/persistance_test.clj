9e75967e275672b1b991bb5dc40d939672fe7bdf
clay austin <clay.austin.ctr@usgs.gov>
2018-06-26 14:10:36 -0500

    fix persistance ns typo for aux data, clean up tests

--- test/mastodon/clj/persistance_test.clj
+++ test/mastodon/clj/persistance_test.clj
@@ -11,18 +11,13 @@
       (is (= {"LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" 200}
              (persist/ingest "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "http://www.iwdshost.com"))))))
 
-;; (deftest status-check-test
-;;   (let [tar "LE07_CU_005015_20021221_20170919_C01_V01_BT.tar"
-;;         tif "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif"]
+(deftest resource-path-test-ard
+  (with-redefs [config/config {:server_type "ard" :ard_host "http://ardhost.com"}]
+    (is (= (persist/resource-path "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif")
+           "http://ardhost.com/ard/etm/ARD_Tile/2002/CU/005/015/LE07_CU_005015_20021221_20170919_C01_V01_BT.tar/LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif"))))
 
-;;   (with-fake-http ["http://iwdshost.org/inventory?only=source&source=LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" {:status 200 :body "ok"}]
-;;     (is (= (persist/status-check tif "http://iwdshost.org" "http://ingesthost.org")
-;;            {"http://ingesthost.org/etm/ARD_Tile/2002/CU/005/015/LE07_CU_005015_20021221_20170919_C01_V01_BT.tar/LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "ok"})))))
+(deftest resource-path-test-aux
+  (with-redefs [config/config {:server_type "aux" :aux_host "http://auxhost.com"}]
+    (is (= (persist/resource-path "AUX_CU_005015_20000731_20171031_V01_ASPECT.tif")
+         "http://auxhost.com/AUX_CU_005015_20000731_20171031_V01.tar/AUX_CU_005015_20000731_20171031_V01_ASPECT.tif"))))
 
-;; (deftest ard-resource-path-test
-;;   (is (= "http://ingestresource/etm/ARD_Tile/2002/CU/005/015/LE07_CU_005015_20021221_20170919_C01_V01_BT.tar/LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif"
-;;        (persist/ard-resource-path "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "http://ingestresource"))))
-
-;; (deftest aux-resource-path-test
-;;   (is (= (persist/aux-resource-path "AUX_CU_005015_20000731_20171031_V01_ASPECT.tif" "http://ingestresource")
-;;          "http://ingestresource/AUX_CU_005015_20000731_20171031_V01.tar/AUX_CU_005015_20000731_20171031_V01_ASPECT.tif")))
