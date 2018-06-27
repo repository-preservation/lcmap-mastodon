092369e9976c78eaacc12f9b53bb3009dcef2c9c
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-06-27 16:06:58 -0500

    add more tests for persistance, validation, and server ns

--- test/mastodon/clj/persistance_test.clj
+++ test/mastodon/clj/persistance_test.clj
@@ -5,19 +5,26 @@
             [mastodon.clj.persistance :as persist]
             [org.httpkit.client :as http]))
 
-(deftest ingest-test
-  (with-fake-http [{:url "http://www.iwdshost.com/inventory" :method :post} {:status 200 :body "ok"}]
-    (with-redefs [config/config {:data_type "ard" :ard_host "foohost.com"}]
-      (is (= {"LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" 200}
-             (persist/ingest "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "http://www.iwdshost.com"))))))
 
 (deftest resource-path-test-ard
-  (with-redefs [config/config {:data_type "ard" :ard_host "http://ardhost.com"}]
+  (with-redefs [config/config {:data_type "ard" :ard_host "http://ardhost.gov"}]
     (is (= (persist/resource-path "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif")
-           "http://ardhost.com/ard/etm/ARD_Tile/2002/CU/005/015/LE07_CU_005015_20021221_20170919_C01_V01_BT.tar/LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif"))))
+           "http://ardhost.gov/ard/etm/ARD_Tile/2002/CU/005/015/LE07_CU_005015_20021221_20170919_C01_V01_BT.tar/LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif"))))
 
 (deftest resource-path-test-aux
-  (with-redefs [config/config {:data_type "aux" :aux_host "http://auxhost.com"}]
+  (with-redefs [config/config {:data_type "aux" :aux_host "http://auxhost.gov"}]
     (is (= (persist/resource-path "AUX_CU_005015_20000731_20171031_V01_ASPECT.tif")
-         "http://auxhost.com/AUX_CU_005015_20000731_20171031_V01.tar/AUX_CU_005015_20000731_20171031_V01_ASPECT.tif"))))
+         "http://auxhost.gov/AUX_CU_005015_20000731_20171031_V01.tar/AUX_CU_005015_20000731_20171031_V01_ASPECT.tif"))))
+
+(deftest ingest-test
+  (with-fake-http [{:url "http://www.iwdshost.gov/inventory" :method :post} {:status 200 :body "ok"}]
+    (with-redefs [config/config {:data_type "ard" :ard_host "foohost.gov"}]
+      (is (= {"LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" 200}
+             (persist/ingest "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "http://www.iwdshost.gov"))))))
+
+(deftest ingest-test-exception
+  (with-redefs [config/config {:data_type "ard" :ard_host "foohost.gov"}]
+    (let [resp (persist/ingest "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" "http://www.iwdshost.gov")]
+      (is (= (set (keys resp)) #{"LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif" :error}))
+      (is (= (get resp "LE07_CU_005015_20021221_20170919_C01_V01_BTB6.tif") 500)))))
 
