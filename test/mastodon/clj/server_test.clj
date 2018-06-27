212b2ec14243dd1ec2703fe8df84175d18908a56
clay austin <clay.austin.ctr@usgs.gov>
2018-06-27 16:06:58 -0500

    add more tests for persistance, validation, and server ns

--- test/mastodon/clj/server_test.clj
+++ test/mastodon/clj/server_test.clj
@@ -23,7 +23,12 @@
     (is (= (server/bulk-ingest {:body {:urls "list,of,ard"}})
            {:status 200 :body '("tif" "tif" "tif")}))))
 
-(deftest available-ard-test
+(deftest bulk-ingest-test-error
+  (with-redefs [config/config {:chipmunk_host "foohost.gov"}]
+    (let [resp (server/bulk-ingest {:body {:data ""}})]
+      (is (string/includes? (get-in resp [:body :error]) "exception with bulk-ingest")))))
+
+(deftest available-ard-test-tile-only
   (with-redefs [file/get-filenames (fn [a b] ["LC08_CU_005015_20130415_20171016_C01_V01_SR.tar" "LT05_CU_005015_19840508_20170912_C01_V01_BT.tar"])] 
     (is (= (server/available-ard "005015")
         '("LC08_CU_005015_20130415_20171016_C01_V01_SRB2.tif"
@@ -35,7 +40,7 @@
           "LC08_CU_005015_20130415_20171016_C01_V01_PIXELQA.tif" 
           "LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif")))))
 
-(deftest filtered-ard-test
+(deftest available-ard-test-dates
   (with-redefs [file/get-filenames (fn [path sfx] filelist)]
     (is (= (server/available-ard "005015" 1983 1994)
            ["LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif"]))))
@@ -48,14 +53,14 @@
 
 (deftest ard-status-test
   (with-redefs [config/config {:data_type "ard" :ard_host "http://ardhost.gov" :chipmunk_host "http://iwdshost.gov"}
-                server/data-tifs         (fn [tile req] ["LC08_CU_005015_20130415_20171016_C01_V01_SRB4.tif" "LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif" "LT04_CU_005015_19821119_20170912_C01_V01_SRB5.tif"])
-                warehouse/ingested-tifs (fn [tile]     ["LC08_CU_005015_20130415_20171016_C01_V01_SRB4.tif" "LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif"])]
+                server/data-tifs (fn [tile req] ["LC08_CU_005015_20130415_20171016_C01_V01_SRB4.tif" "LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif" "LT04_CU_005015_19821119_20170912_C01_V01_SRB5.tif"])
+                warehouse/ingested-tifs (fn [tile] ["LC08_CU_005015_20130415_20171016_C01_V01_SRB4.tif" "LT05_CU_005015_19840508_20170912_C01_V01_BTB6.tif"])]
     (is (= (server/get-status "005015" {})
            {:status 200 :body {:missing ["LT04_CU_005015_19821119_20170912_C01_V01_SRB5.tif"] :ingested 2}}))))
 
 (deftest aux-status-test
   (with-redefs [config/config {:data_type "aux" :ard_host "http://ardhost.gov" :chipmunk_host "http://iwdshost.gov"}
-                server/data-tifs         (fn [tile req] ["AUX_CU_005015_20000731_20171031_V01_ASPECT.tif" "AUX_CU_005015_20000731_20171031_V01_POSIDEX.tif" "AUX_CU_005015_20000731_20171031_V01_TRENDS.tif"])
+                server/data-tifs (fn [tile req] ["AUX_CU_005015_20000731_20171031_V01_ASPECT.tif" "AUX_CU_005015_20000731_20171031_V01_POSIDEX.tif" "AUX_CU_005015_20000731_20171031_V01_TRENDS.tif"])
                 warehouse/ingested-tifs (fn [tile] ["AUX_CU_005015_20000731_20171031_V01_ASPECT.tif"])]
     (is (= (server/get-status "005015" {})
            {:status 200 :body {:missing ["AUX_CU_005015_20000731_20171031_V01_TRENDS.tif" "AUX_CU_005015_20000731_20171031_V01_POSIDEX.tif"] :ingested 1}}))))
--- test/mastodon/clj/validation_test.clj
+++ test/mastodon/clj/validation_test.clj
@@ -3,30 +3,52 @@
   (:require [clojure.test :refer :all] 
             [mastodon.clj.validation :as validation]))
 
-(deftest http-accessible?-test
+(deftest http?-test
   (with-fake-http [{:url "http://foo.com" :method :get} {:status 200 :body "okay"}]
     (is (= true (validation/http? "http://foo.com" "iwds")))))
 
-(deftest http-accessible?-false-test
+(deftest http?-false-test
   (with-fake-http [{:url "http://foo.com" :method :get} {:status 403 :body "forbidden"}]
     (is (= false (validation/http? "http://foo.com" "iwds")))))
 
-(deftest present?-test
+(deftest http?-exception-test
+  (is (= false (validation/http? nil "iwds"))))
+
+(deftest present?-true-test
   (is (= (validation/present? 9 "foo") true)))
 
-(deftest match?-test
+(deftest present?-false-test
+  (is (= (validation/present? nil "foo") false)))
+
+(deftest match?-true-test
   (is (= (validation/match? #"[0-9]{6}" "005015" "TileID") true)))
 
-(deftest is-int?-test
+(deftest match?-false-test
+  (is (= (validation/match? #"[0-9]{6}" "005015xx" "TileID") false)))
+
+(deftest match?-exception-test
+  (is (= (validation/match? nil "005015" "TileID") false)))
+
+(deftest int?-true-test
   (is (= (validation/int? 9 "Foo") true)))
 
-(deftest validate-cli-test
+(deftest int?-false-test
+  (is (= (validation/int? nil "Foo") false)))
+
+(deftest validate-cli-ard-test
   (with-redefs [validation/http? (fn [x y] true)]
     (is (= (validation/validate-cli "005015" {:chipmunk_host "iwdshost" :ard_host "ardhost" :partition_level 10 :data_type "ard"}) true))))
 
-(deftest validate-server-test
+(deftest validate-cli-aux-test
   (with-redefs [validation/http? (fn [x y] true)]
-    (is (= (validation/validate-server {:data_type "aux" :chipmunk_host "iwdshost" :ard_host "ardhost" :aux_host "auxhost" :partition_level 10 :ard_path "/tmp/foo/"} ) true))))
+    (is (= (validation/validate-cli "005015" {:chipmunk_host "iwdshost" :ard_host "ardhost" :aux_host "auxhost" :partition_level 10 :data_type "aux"}) true))))
 
+(deftest validate-ard-server-test
+  (with-redefs [validation/http? (fn [x y] true)]
+    (is (= (validation/validate-server {:data_type "ard" :chipmunk_host "iwdshost" :ard_host "ardhost" :nemo_host "nemohost" :partition_level 10 :ard_path "/tmp/foo/"} ) true))))
+
+(deftest validate-aux-server-test
+  (with-redefs [validation/http? (fn [x y] true)]
+    (is (= (validation/validate-server {:data_type "aux" :chipmunk_host "iwdshost" :ard_host "ardhost" :aux_host "auxhost" :nemo_host "nemohost" :partition_level 10} ) true))))
 
 
