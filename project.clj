b4de3580128e260e1c9dc1e7709d83a393ff022b
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-02-28 13:41:12 -0600

    update nginx conf for new endpoint

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lcmap-mastodon "3.1.3"
+(defproject lcmap-mastodon "3.2.4"
   :description "Functions for LCMAP data curation"
   :url "https://eros.usgs.gov"
   :license {:name "Unlicense"
